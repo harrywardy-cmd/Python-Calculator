68387a8c85239cb5ecd045a61cd9c3b1c2c7a990
harrywardy-cmd <harrywardy303@gmail.com>
2024-12-01 18:25:07 +1100

    created frames for UI

--- calculator.py
+++ calculator.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 
+LIGHT_GRAY = "#F5F5F5"
 class Calculator:
     def __init__(self):
         self.window = tk.Tk()
@@ -7,10 +8,22 @@ class Calculator:
         self.window.resizable(0,0)
         self.window.title("Calculator")
 
-    
+        self.display_frame = self.create_display_frame()
+        self.button_frame = self.create_button_frame()
+
+    def create_display_frame(self):
+        frame = tk.Frame(self.window, height=221, bg=LIGHT_GRAY)
+        frame.pack(expand=True, fill="both")
+        return frame
+
+    def create_button_frame(self):
+        frame = tk.Frame(self.window)
+        frame.pack(expand=True, fill="both")
+        return frame
+
     def run(self):
         self.window.mainloop()
 
-if __name__ == "main":
+if __name__ == "__main__":
     calc = Calculator()
     calc.run()
