b306a8013b3f0d2c3e92e09a584090a8f6967b09
harrywardy-cmd <harrywardy303@gmail.com>
2025-03-20 11:14:07 +1100

    added x2 and sqrt fucntions

--- calculator.py
+++ calculator.py
@@ -54,6 +54,8 @@ class Calculator:
     def create_special_buttons(self):
         self.create_clear_button()
         self.create_equals_button()
+        self.create_sqrt_button()
+        self.create_square_button()
 
     def create_display_lables(self):
         total_label = tk.Label(self.display_frame, text=self.total_expression, anchor=tk.E, bg=LIGHT_GRAY, fg=LABLE_COLOR, padx= 24, font=SMALL_FONT_STYLE)
@@ -100,7 +102,24 @@ class Calculator:
 
     def create_clear_button(self):
         button = tk.Button(self.buttons_frame, text="C", bg=OFF_WHITE, fg=LABLE_COLOR, font=DEFAULT_FONT_STYLE, borderwidth=0,command=self.clear)
-        button.grid(row=0,column=1, columnspan=3,sticky=tk.NSEW)
+        button.grid(row=0,column=1, sticky=tk.NSEW)
+
+    def square(self):
+        self.current_expression = str(eval(f"{self.current_expression}**2"))
+        self.update_lable()
+
+    def create_square_button(self):
+        button = tk.Button(self.buttons_frame, text="x\u00b2", bg=OFF_WHITE, fg=LABLE_COLOR, font=DEFAULT_FONT_STYLE, borderwidth=0,command=self.square)
+        button.grid(row=0,column=2, sticky=tk.NSEW)
+
+    def sqrt(self):
+        self.current_expression = str(eval(f"{self.current_expression}**0.5"))
+        self.update_lable()
+
+    def create_sqrt_button(self):
+        button = tk.Button(self.buttons_frame, text="\u221ax", bg=OFF_WHITE, fg=LABLE_COLOR, font=DEFAULT_FONT_STYLE, borderwidth=0,command=self.sqrt)
+        button.grid(row=0,column=3, sticky=tk.NSEW)
+    
 
     #this will handle the logic with pythons eval() function
     def evaluate(self):
